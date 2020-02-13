fe909f4297440e9ee6ab46727532ae150f3f882e
Aleksandar Vasiljevic <aleksandarvasiljevic111@gmail.com>
2020-02-13 14:17:24 +0100

    heroku depl 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
 import thunk from 'redux-thunk'
 
 import rootReducer from './root-reducer'
 
-const middlewares = [logger, thunk]
+const middlewares = [thunk]
 
 // if (process.env.NODE_ENV === 'development') {
 //   middlewares.push(logger)
