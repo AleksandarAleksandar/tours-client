aa7c9bc8f803ee8c5d3888b7c5ebf63d186d0552
Aleksandar Vasiljevic <aleksandarvasiljevic111@gmail.com>
2020-02-13 13:46:12 +0100

    heroku deply 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux/store'
 
 ReactDOM.render(
-  <div>bla bla bla!</div>
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <PersistGate persistor={persistor}>  */}
+        <App />
+      {/* </PersistGate>  */}
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
 
 
